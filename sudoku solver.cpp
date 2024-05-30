d25315f684a91e8d04849ec3b11f90024baa9008
srihitha Pulapa <124448232+srihitha2005@users.noreply.github.com>
2024-05-30 18:48:31 +0530

    Update sudoku solver.cpp

--- sudoku solver.cpp
+++ sudoku solver.cpp
@@ -1,91 +1,91 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
-#define N 9
+const int SIZE = 9;
 
-//printing the array
-void print(int arr[N][N])
-{
-    for (int i = 0; i < N; i++)
-    {
-        for (int j = 0; j < N; j++)
-            cout << arr[i][j] << " ";
+void printGrid(const vector<vector<int>>& grid) {
+    for (int i = 0; i < SIZE; i++) {
+        for (int j = 0; j < SIZE; j++) {
+            cout << grid[i][j] << " ";
+        }
         cout << endl;
     }
 }
 
-//checking if the state of board is valid or not
-bool isSafe(int grid[N][N], int row, int col, int num)
-{
-    for (int x = 0; x <= 8; x++)
-        if (grid[row][x] == num)
+bool isValid(const vector<vector<int>>& grid, int row, int col, int num) {
+    // Check the row
+    for (int x = 0; x < SIZE; x++) {
+        if (grid[row][x] == num) {
             return false;
+        }
+    }
 
-    for (int x = 0; x <= 8; x++)
-        if (grid[x][col] == num)
+    // Check the column
+    for (int x = 0; x < SIZE; x++) {
+        if (grid[x][col] == num) {
             return false;
+        }
+    }
 
-    int startRow = row - row % 3, startCol = col - col % 3;
-
-    for (int i = 0; i < 3; i++)
-        for (int j = 0; j < 3; j++)
-            if (grid[i + startRow][j + startCol] == num)
+    // Check the 3x3 box
+    int boxRow = row - row % 3;
+    int boxCol = col - col % 3;
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            if (grid[boxRow + i][boxCol + j] == num) {
                 return false;
+            }
+        }
+    }
 
     return true;
 }
 
-//final solving
-bool solveSudoku(int grid[N][N], int row, int col)
-{
-    //if already completely filled return true
-    if (row == N - 1 && col == N)
+bool solveSudoku(vector<vector<int>>& grid, int row, int col) {
+    if (row == SIZE - 1 && col == SIZE) {
         return true;
+    }
 
-    if (col == N) {
+    if (col == SIZE) {
         row++;
         col = 0;
     }
 
-    if (grid[row][col] > 0)
+    if (grid[row][col] > 0) {
         return solveSudoku(grid, row, col + 1);
+    }
 
-    for (int num = 1; num <= N; num++)
-    {
-
-        if (isSafe(grid, row, col, num))
-        {
-
+    for (int num = 1; num <= SIZE; num++) {
+        if (isValid(grid, row, col, num)) {
             grid[row][col] = num;
-
-            if (solveSudoku(grid, row, col + 1))
+            if (solveSudoku(grid, row, col + 1)) {
                 return true;
+            }
         }
-
         grid[row][col] = 0;
     }
+
     return false;
 }
 
-int main()
-{
-    int grid[N][N];
+int main() {
+    vector<vector<int>> grid(SIZE, vector<int>(SIZE));
 
-    cout << "Enter the Sudoku grid (0 for empty cells):" << endl;
-    for (int i = 0; i < N; i++)
-    {
-        for (int j = 0; j < N; j++)
-        {
+    cout << "Enter the Sudoku grid (0 for empty cells):\n";
+    for (int i = 0; i < SIZE; i++) {
+        for (int j = 0; j < SIZE; j++) {
             cin >> grid[i][j];
         }
     }
 
-    cout<<"Solved sudoku : \n";
-    if (solveSudoku(grid, 0, 0))
-        print(grid);
-    else
-        cout << "no solution exists " << endl;
+    cout << "Solved Sudoku:\n";
+    if (solveSudoku(grid, 0, 0)) {
+        printGrid(grid);
+    } else {
+        cout << "No solution exists.\n";
+    }
 
     return 0;
 }
